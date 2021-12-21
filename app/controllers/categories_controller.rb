140a4bbc61bd9b5dbc8dcd20048696100c283420
tiagorahal <rahal.aires@gmail.com>
2021-12-21 13:06:32 -0300

    Fix linters

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,27 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = current_user.categories.includes(:expenses)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @expenses = @category.expenses.order(:created_at)
+  end
+
+  def new
+    @errors = params[:errors]
+  end
+
+  def create
+    @user = current_user
+    @category = current_user.categories.new(name: params[:name], icon: params[:icon])
+    if @category.save
+      flash[:notice] = 'Category created successfully'
+      redirect_to '/user'
+    else
+      flash[:notice] = 'Something is wrong, please look at the errors'
+      @errors = @category.errors.full_messages
+      redirect_to categories_new_path(errors: @errors)
+    end
+  end
 end
